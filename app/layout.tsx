65fd8f40ef1181338e840e7969cdd4036a86e515
Dishant Sharma <dishu5570@gmail.com>
2025-03-20 17:22:43 +0530

    Add Open Graph and Twitter meta tags for improved SEO and sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
+import Head from 'next/head';
 import { headers } from 'next/headers';
+import Script from 'next/script';
 
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { LOGOUT_ENDPOINT } from '@/lib/constants';
@@ -50,6 +52,42 @@ const RootLayout = async ({
 
   return (
     <html suppressHydrationWarning lang="en">
+      <Head>
+        <meta content="website" property="og:type" />
+        <meta content="https://redirect.lazyweb.rocks/" property="og:url" />
+        <meta
+          content="Domain Redirecting Service hassle-free - redirect.lazyweb.rocks"
+          property="og:title"
+        />
+        <meta
+          content="Get peace of mind when redirecting your domains without the burden of hosting them. We are a domain redirect service with full HTTPS support and API compatibility. Enter your domain names and we'll take care of the rest."
+          property="og:description"
+        />
+        <meta
+          content="https://cdn.jsdelivr.net/gh/dishant0406/images-repo@master/redirect.png"
+          property="og:image"
+        />
+
+        <meta content="summary_large_image" property="twitter:card" />
+        <meta content="https://redirect.lazyweb.rocks/" property="twitter:url" />
+        <meta
+          content="Domain Redirecting Service hassle-free - redirect.lazyweb.rocks"
+          property="twitter:title"
+        />
+        <meta
+          content="Get peace of mind when redirecting your domains without the burden of hosting them. We are a domain redirect service with full HTTPS support and API compatibility. Enter your domain names and we'll take care of the rest."
+          property="twitter:description"
+        />
+        <meta
+          content="https://cdn.jsdelivr.net/gh/dishant0406/images-repo@master/redirect.png"
+          property="twitter:image"
+        />
+      </Head>
+      <Script
+        defer
+        data-website-id="15e4102e-1090-4e73-951b-ae039e7cf88c"
+        src="https://cloud.umami.is/script.js"
+      />
       <body
         className={`${geistSans.variable} dark ${geistMono.variable} antialiased`}
         data-body={encodedUserDetailsBase64}
